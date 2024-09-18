40635d57a628f4602d66697b9a544cac3b14ef1b
shtemberg.v <v.shtemberg@bk.ru>
2024-09-18 20:03:56 +0300

    style

--- src/components/PostLists.vue
+++ src/components/PostLists.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
-        <h2>Посты</h2>
-        <ul>
-            <li v-for="post in posts" :key="post.id">
+        <h2 class="title">Посты</h2>
+        <ul class="item">
+            <li class="lists" v-for="post in posts" :key="post.id">
                 {{ post.title }} 
             </li>
         </ul>
@@ -52,3 +52,20 @@ export default {
 
 
 </script>
+
+<style scoped>
+    .title {
+        text-align: center;
+        color: rgb(255, 180, 193);
+        font-size: 30px;
+    }
+    .item {
+        list-style-type: none;
+        padding: 50px;
+        color: rgb(255, 27, 171)
+        
+    }
+    .lists {
+        padding: 10px;
+    }
+</style>
